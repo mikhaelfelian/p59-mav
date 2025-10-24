e15ea148297f8c1d3c7dbf65885c3645c80487a4
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-10-24 08:20:34 +0700

    Update UserModel.php
    Adjusting UserModel into ci 4.3.1 standard

--- app/Models/Builtin/UserModel.php
+++ app/Models/Builtin/UserModel.php
@@ -4,6 +4,50 @@ use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
 
 class UserModel extends \App\Models\BaseModel
 {
+	protected $table = 'user';
+	protected $primaryKey = 'id_user';
+	protected $useAutoIncrement = true;
+	protected $returnType = 'array';
+	protected $useSoftDeletes = false;
+	protected $protectFields = true;
+	protected $allowedFields = [
+		'nama',
+		'email', 
+		'username',
+		'password',
+		'status',
+		'verified',
+		'avatar',
+		'default_page_type',
+		'default_page_id_role',
+		'default_page_id_module',
+		'default_page_url'
+	];
+
+	// Dates
+	protected $useTimestamps = false;
+	protected $dateFormat = 'datetime';
+	protected $createdField = 'created_at';
+	protected $updatedField = 'updated_at';
+	protected $deletedField = 'deleted_at';
+
+	// Validation
+	protected $validationRules = [];
+	protected $validationMessages = [];
+	protected $skipValidation = false;
+	protected $cleanValidationRules = true;
+
+	// Callbacks
+	protected $allowCallbacks = true;
+	protected $beforeInsert = [];
+	protected $afterInsert = [];
+	protected $beforeUpdate = [];
+	protected $afterUpdate = [];
+	protected $beforeFind = [];
+	protected $afterFind = [];
+	protected $beforeDelete = [];
+	protected $afterDelete = [];
+
 	public function getListUsers($where) {
 		
 		// Get user
